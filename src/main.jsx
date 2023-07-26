8d44c047462b3095fcc6142559c9f00c30dbd2c3
Jayspart45 <jayamstrikerjrk@gmail.com>
2023-07-26 11:39:07 +0530

    analytic vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { inject } from "@vercel/analytics";
 import { Analytics } from "@vercel/analytics/react";
 import LoginProvider from "./components/Context/LoginProvider";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
